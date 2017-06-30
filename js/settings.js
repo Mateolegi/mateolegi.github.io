ffa6f0120e4ca04e278b6a8739657aa560b0527e
Alex <alexius9000@gmail.com>
2017-06-29 23:51:51 -0600

    i added the footer section
    Wating for the content section and contact

--- js/settings.js
+++ js/settings.js
@@ -1,15 +1,32 @@
 $(document).ready(function(){
 	altura();
 
+
+  $("footer").footerReveal({zIndex: -101 });
 });
+
+ $("#sk1").click(function(){
+  setear1();
+  $("#design").animateCss('fadeInLeft');
+ });
+
+
+ $("#sk2").click(function(){
+  setear2();
+  $("#database").animateCss('fadeInRight');
+ });
+
+
+ $("#sk3").click(function(){
+  setear3();
+ });
+
 
 
 /*
 *Esta funcion permite dar valores de css 
 *a la barra de navegacion cuando se hace scroll
 */
-
-
 var flag = true;
 $(window).scroll(function(){
     var scroll = $(document).scrollTop();
@@ -86,3 +103,78 @@ $('a[href*="#"]')
       }
     }
   });
+
+
+
+
+ /*
+ **************************************************************
+ * this functions are using for setting the buttons by defoult
+ * usig the skill navbar buttons
+ ***************************************************************
+ */ 
+
+function setear1(){
+    $("#c1").removeClass("unactive");
+    $("#c2").removeClass("active");
+    $("#c3").removeClass("active");
+
+    $("#c1").addClass("active");
+    $("#c2").addClass("unactive");
+    $("#c3").addClass("unactive");
+
+
+// Setting Content
+    $("#design").removeClass("show");
+    $("#database").removeClass("show");
+
+    $("#design").addClass("show");
+    $("#database").addClass("none");
+    
+
+}
+function setear2(){
+    $("#c1").removeClass("active");
+    $("#c2").removeClass("unactive");
+    $("#c3").removeClass("active");
+
+    $("#c1").addClass("unactive");
+    $("#c2").addClass("active");
+    $("#c3").addClass("unactive");
+
+
+    // Setting Content
+    $("#design").removeClass("show");
+    $("#database").removeClass("none");
+
+    $("#database").addClass("show");
+    $("#design").addClass("none");
+    
+
+}
+function setear3(){
+    $("#c1").removeClass("active");
+    $("#c2").removeClass("active");
+    $("#c3").removeClass("unactive");
+
+    $("#c1").addClass("unactive");
+    $("#c2").addClass("unactive");
+    $("#c3").addClass("active");
+}
+
+
+
+/*
+*******************************************
+* Animate css code 
+**********************************************
+*/
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});

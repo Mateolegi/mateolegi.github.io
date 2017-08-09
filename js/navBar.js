a0ccc695ba4109a61c74d93b08633a05526da5f9
Mateo Leal <mateolegi@gmail.com>
2017-08-08 23:32:23 -0500

    Correcciones de identado

--- js/navBar.js
+++ js/navBar.js
@@ -1,15 +1,15 @@
 var clicked = true;
-$("#menuButton").click(function(){
-  if (clicked){
-      $("#pageWrap").css({
-        "transform":' translateX(180px)'
-      });
-      clicked = false;
 
-}else{
-  $("#pageWrap").css({
-    "transform":' translateX(0px)'
-  });
+$("#menuButton").click(function() {
+  if (clicked) {
+    $("#pageWrap").css({
+      "transform":' translateX(180px)'
+    });
+    clicked = false;
+  } else {
+    $("#pageWrap").css({
+      "transform":' translateX(0px)'
+    });
   clicked = true;
-}
+  }
 });
--- js/settings.js
+++ js/settings.js
@@ -1,203 +1,179 @@
 // Global Variables
-$(document).ready(function(){
+$(document).ready(function() {
 	altura();
 });
 
-
-
-
-/*
-*Esta funcion setea la altura minimo del banner
-*con respecto a la pantalla desde donde se vé
-*/
-
-function altura(){
+/**
+ * Esta función setea la altura mínima del banner
+ * con respecto a la pantalla desde donde se ve
+ */
+function altura() {
   var altura= $(window).height();
   $("#bannerContainer").css({"min-height" : altura+"px"});
   $("#bannerRow").css({"min-height": altura+"px"});
 }
 
-
- $("#sk1").click(function(){
+ $("#sk1").click(function() {
   setear1();
   $("#design").animateCss('fadeInLeft');
  });
 
 
- $("#sk2").click(function(){
+ $("#sk2").click(function() {
   setear2();
   $("#database").animateCss('fadeInRight');
  });
 
- $("#sk3").click(function(){
+ $("#sk3").click(function() {
   setear3();
   $("#backend").animateCss('fadeInLeft');
  });
 
 
 
-/*
-*Esta funcion permite dar valores de css
-*a la barra de navegacion cuando se hace scroll
-*/
-
+/**
+ * Esta función permite dar valores de css
+ * a la barra de navegación cuando se hace scroll
+ */
 var flag = true;
-$(window).scroll(function(){
-    landingElement();
-		quoteAnimation();
-    navbar();
+$(window).scroll(function() {
+  landingElement();
+	quoteAnimation();
+  navbar();
 })
 
-function navbar(){
+function navbar() {
   var wScroll = $(document).scrollTop();
   var skill = $('#skill').offset().top - ($(window).height() * .1);
 
-
-  if (wScroll > (skill)){
-
-
-      $("#navContainer").css({ 
-        "background-color": "rgba(44,62,80,0.7)",
-        "height": "50px"
-      });
-
-      $("#navbarCollapse").css({"padding-top" : "0px"});
-      $(".navbar-header").css({"padding-top" : "0px"});
-  }else{
-      $("#navContainer").css({ "background-color": "rgba(205,205,205,.0)"});
-      $("#navbarCollapse").css({"padding-top" : "20px"});
-      $(".navbar-header").css({"padding-top" : "20px"});
+  if (wScroll > skill){
+    $("#navContainer").css({ 
+      "background-color": "rgba(44,62,80,0.7)",
+      "height": "50px"
+    });
+    $("#navbarCollapse").css({"padding-top" : "0px"});
+    $(".navbar-header").css({"padding-top" : "0px"});
+  } else {
+    $("#navContainer").css({ "background-color": "rgba(205,205,205,.0)"});
+    $("#navbarCollapse").css({"padding-top" : "20px"});
+    $(".navbar-header").css({"padding-top" : "20px"});
   }
 }
 
+function landingElement() {
+  var wScroll = $(document).scrollTop();
+  var skill = $('#skill').offset().top - ($(window).height() * .3);
+  if (wScroll > skill ) {
+    $("#left-skill").css({
+      'transform': 'translateX(0)',
+      'opacity':1
+    });
+  } else {
+    $("#left-skill").css({
+      'transform': 'translateX(40px)',
+      'opacity' : 0
+    });
+  }
+}
 
-function landingElement(){
-    var wScroll = $(document).scrollTop();
-    var skill = $('#skill').offset().top - ($(window).height() * .3);
-    if(wScroll > skill ) {
-      $("#left-skill").css({
-          'transform': 'translateX(0)',
-          'opacity':1
-        });
-    }else{
-      $("#left-skill").css({
-        'transform': 'translateX(40px)',
-        'opacity' : 0
-     });
-    }
+ /**
+  * this functions are using for setting the buttons by default
+  * using the skill navbar buttons
+  */
+function setear1() {
+  $("#c1").removeClass("unactive");
+  $("#c2").removeClass("active");
+  $("#c3").removeClass("active");
+
+  $("#c1").addClass("active");
+  $("#c2").addClass("unactive");
+  $("#c3").addClass("unactive");
+
+  // Setting Content
+  $("#design").removeClass("none");
+  $("#database").removeClass("show");
+  $("#backend").removeClass("show");
+
+  $("#design").addClass("show");
+  $("#database").addClass("none");
+  $("#backend").addClass("none");
+}
+
+function setear2() {
+  $("#c1").removeClass("active");
+  $("#c2").removeClass("unactive");
+  $("#c3").removeClass("active");
+
+  $("#c1").addClass("unactive");
+  $("#c2").addClass("active");
+  $("#c3").addClass("unactive");
+
+  // Setting Content
+  $("#design").removeClass("show");
+  $("#database").removeClass("none");
+  $("#backend").removeClass("show");
+
+  $("#database").addClass("show");
+  $("#design").addClass("none");
+  $("#backend").addClass("none");
+}
+
+function setear3() {
+  $("#c1").removeClass("active");
+  $("#c2").removeClass("active");
+  $("#c3").removeClass("unactive");
+
+  $("#c1").addClass("unactive");
+  $("#c2").addClass("unactive");
+  $("#c3").addClass("active");
+
+  // Setting Content
+  $("#design").removeClass("show");
+  $("#database").removeClass("show");
+  $("#backend").removeClass("none");
+
+  $("#backend").addClass("show");
+  $("#design").addClass("none");
+  $("#database").addClass("none");
 }
 
 
 
- /*
- **************************************************************
- * this functions are using for setting the buttons by defoult
- * usig the skill navbar buttons
- ***************************************************************
- */
-
-function setear1(){
-    $("#c1").removeClass("unactive");
-    $("#c2").removeClass("active");
-    $("#c3").removeClass("active");
-
-    $("#c1").addClass("active");
-    $("#c2").addClass("unactive");
-    $("#c3").addClass("unactive");
-
-
-// Setting Content
-    $("#design").removeClass("none");
-    $("#database").removeClass("show");
-    $("#backend").removeClass("show");
-
-
-    $("#design").addClass("show");
-    $("#database").addClass("none");
-    $("#backend").addClass("none");
-
-
-}
-function setear2(){
-    $("#c1").removeClass("active");
-    $("#c2").removeClass("unactive");
-    $("#c3").removeClass("active");
-
-    $("#c1").addClass("unactive");
-    $("#c2").addClass("active");
-    $("#c3").addClass("unactive");
-
-
-    // Setting Content
-    $("#design").removeClass("show");
-    $("#database").removeClass("none");
-    $("#backend").removeClass("show");
-
-    $("#database").addClass("show");
-    $("#design").addClass("none");
-    $("#backend").addClass("none");
-
-
-}
-function setear3(){
-    $("#c1").removeClass("active");
-    $("#c2").removeClass("active");
-    $("#c3").removeClass("unactive");
-
-    $("#c1").addClass("unactive");
-    $("#c2").addClass("unactive");
-    $("#c3").addClass("active");
-
-    // Setting Content
-    $("#design").removeClass("show");
-    $("#database").removeClass("show");
-    $("#backend").removeClass("none");
-
-    $("#backend").addClass("show");
-    $("#design").addClass("none");
-    $("#database").addClass("none");
-}
-
-
-
-function quoteAnimation(){
+function quoteAnimation() {
 	var wScroll = $(document).scrollTop();
 	var extra = $('#extraInfo').offset().top - ($(window).height() * .6);
 
 	if (wScroll > extra) {
-			$("#inside").css({
-          'transform': 'translateX(0)',
-          'opacity':1
-        });
+		$("#inside").css({
+      'transform': 'translateX(0)',
+      'opacity':1
+    });
 
-				$("#author").css({
-	          'transform': 'translateX(0)',
-	          'opacity':1
-	        });
-	}else {
+		$("#author").css({
+      'transform': 'translateX(0)',
+      'opacity':1
+	   });
+	} else {
 		$("#inside").css({
 			'transform': 'translateX(40px)',
-			'opacity' : 0
-	 });
+			'opacity': 0
+    });
 
-	 $("#author").css({
-			 'transform': 'translateX(-50px)',
-			 'opacity':1
-		 });
+    $("#author").css({
+			'transform': 'translateX(-50px)',
+			'opacity':1
+		});
 	}
-
 }
 
-/*
-*******************************************
-* Animate css code
-**********************************************
-*/
+/**
+ * Animate css code
+ */
 $.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
+  animateCss: function (animationName) {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    this.addClass('animated ' + animationName).one(animationEnd, function() {
+      $(this).removeClass('animated ' + animationName);
+    });
+  }
 });
